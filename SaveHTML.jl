314854a3bc3b988230c3ef9fa7079638516bd192
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-11 06:35:27 +0600

    SaveHTML.jl

--- SaveHTML.jl
+++ SaveHTML.jl
@@ -1,1 +1,9 @@
+# save Table as HTML file
 
+using PrettyTables
+
+function savehtml(filename, data)
+    open("$filename.html", "w") do f
+        pretty_table(f, data, backend = :html)
+    end
+end
